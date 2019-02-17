eb0fffbd65ef747aa2b525c6a3c8028c648e35c6
Marcio <Marcio@Marcio-PC>
2019-02-17 14:24:07 -0300

    COMMIT DO MARCINHO

--- src/Controller/FuncionarioController.java
+++ src/Controller/FuncionarioController.java
@@ -2,41 +2,42 @@
 package Controller;
 
 import Models.DAO;
+import java.util.Date;
 
 public class FuncionarioController {
     
-    public String salvarFuncionario( String cnpjFornecedor, String nomeFornecedor, 
-            String emailFornecedor,String representanteFornecedor, String emailRepresentante, String ramoFornecedor, 
-            String telFornecedor, String celFornecedor, int fkEnderecoFornecedor ){
+    public String salvarFuncionario( String nomeFuncionario,  String cpfFuncionario,  
+            String rgFuncionario,  String telFuncionario,  String celFuncionario,  String emailFuncionario,  
+            Date dataNascFuncionario,  int fkEnderecoFuncionario ){
         DAO dao = new DAO();
-        dao.fornecedor.setCnpjFornecedor(cnpjFornecedor);
-        dao.fornecedor.setNomeFornecedor(nomeFornecedor);
-        dao.fornecedor.setEmailFornecedor(emailFornecedor);
-        dao.fornecedor.setRepresentanteFornecedor(representanteFornecedor);
-        dao.fornecedor.setEmailRepresentante(emailRepresentante);
-        dao.fornecedor.setRamoFornecedor(ramoFornecedor);
-        dao.fornecedor.setTelFornecedor(telFornecedor);
-        dao.fornecedor.setCelFornecedor(celFornecedor);
-        dao.fornecedor.setFkEnderecoFornecedor(fkEnderecoFornecedor);
+        dao.funcionario.setNomeFuncionario(nomeFuncionario);
+        dao.funcionario.setCpfFuncionario(cpfFuncionario);
+        dao.funcionario.setRgFuncionario(rgFuncionario);
+        dao.funcionario.setTelFuncionario(telFuncionario);
+        dao.funcionario.setCelFuncionario(celFuncionario);
+        dao.funcionario.setEmailFuncionario(emailFuncionario);
+        dao.funcionario.setDataNascFuncionario(dataNascFuncionario);
+        dao.funcionario.setFkEnderecoFuncionario(fkEnderecoFuncionario);
+       
         String msgInclusao = dao.atualizar(DAO.INCLUSAOFUNCIONARIO);
         return msgInclusao;
     }
      
-    public String alterarFuncionario(String IdFornecedor, String cnpjFornecedor, String nomeFornecedor, 
-            String emailFornecedor,String representanteFornecedor, String emailRepresentante, String ramoFornecedor, 
-            String telFornecedor, String celFornecedor, int fkEnderecoFornecedor ){
+    public String alterarFuncionario(String idFuncionario,  String nomeFuncionario,  String cpfFuncionario,  
+            String rgFuncionario,  String telFuncionario,  String celFuncionario,  String emailFuncionario,  
+            Date dataNascFuncionario,  int fkEnderecoFuncionario ){
         DAO dao = new DAO();
-        dao.fornecedor.setIdFornecedor(IdFornecedor);
-        dao.fornecedor.setCnpjFornecedor(cnpjFornecedor);
-        dao.fornecedor.setNomeFornecedor(nomeFornecedor);
-        dao.fornecedor.setEmailFornecedor(emailFornecedor);
-        dao.fornecedor.setRepresentanteFornecedor(representanteFornecedor);
-        dao.fornecedor.setEmailRepresentante(emailRepresentante);
-        dao.fornecedor.setRamoFornecedor(ramoFornecedor);
-        dao.fornecedor.setTelFornecedor(telFornecedor);
-        dao.fornecedor.setCelFornecedor(celFornecedor);
-        dao.fornecedor.setFkEnderecoFornecedor(fkEnderecoFornecedor);
-      
+        dao.funcionario.setIdFuncionario(idFuncionario);
+        dao.funcionario.setNomeFuncionario(nomeFuncionario);
+        dao.funcionario.setCpfFuncionario(cpfFuncionario);
+        dao.funcionario.setRgFuncionario(rgFuncionario);
+        dao.funcionario.setTelFuncionario(telFuncionario);
+        dao.funcionario.setCelFuncionario(celFuncionario);
+        dao.funcionario.setEmailFuncionario(emailFuncionario);
+        dao.funcionario.setDataNascFuncionario(dataNascFuncionario);
+        dao.funcionario.setFkEnderecoFuncionario(fkEnderecoFuncionario);
+     
+       
         String msgInclusao = dao.atualizar(DAO.ALTERACAOFUNCIONARIO);
         return msgInclusao;
     }
